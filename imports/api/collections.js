26ba4f2227197e82f03a6a2482a663b99773eaaa
BuildTools <unconfigured@null.spigotmc.org>
2016-09-23 23:31:28 +0200

    Initialisation

---
+++ imports/api/collections.js
@@ -0,0 +1,7 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Writings = new Mongo.Collection('writings');
+export const Accounts = new Mongo.Collection('accounts');
+export const Differences = new Mongo.Collection('differences');
+export const DifferencesRules = new Mongo.Collection('DifferencesRules');
+export const Logs = new Mongo.Collection('logs');
