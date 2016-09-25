1f310b7901f62b633cf3b31406c61fca18f3e753
Samy <samy.laumonier@gmail.com>
2016-09-25 16:47:11 +0200

    Parsing du fichier à importer

--- client/ui/collections/writings.js
+++ client/ui/collections/writings.js
@@ -1,7 +1,48 @@
 import moment from 'moment';
+import Papa from 'papaparse';
+
+Template.writingsImport.onCreated(function () {
+    this.data.loading = new ReactiveVar(false);
+    this.data.rows = new ReactiveVar([]);
+});
 
 Template.writingsImportPrewiew.helpers({
-    getDate: (date) => {
-        return moment(date, 'YYYYMMDD').format('DD MMM YYYY');
+    getDate: date => {
+        return date && moment(date, 'YYYYMMDD').format('DD/MM/YYYY');
+    }
+});
+
+Template.writingsImport.events({
+    'change #file': function (event) {
+        const self = this;
+        const files = event.target.files;
+
+        if (files && files.length) {
+            self.loading.set(true);
+            console.log('start');
+
+            Papa.parse(files[0], {
+                delimiter: '\t',
+                skipEmptyLines: true,
+                error: (err, file, input, reason) => {
+                    self.loading.set(false);
+                    console.log('parser error', err, reason);
+                    alert(reason);
+                },
+                complete: results => {
+                    self.loading.set(false);
+                    if (results.errors.length) {
+                        console.log('parser error', results.errors);
+                        alert('Error');
+                    } else {
+                        console.log('completed');
+                        const rows = results.data.map(row => row.map(column => column.trim()));
+                        rows.shift();
+                        console.log('end');
+                        self.rows.set(rows);
+                    }
+                }
+            });
+        }
     }
 });
